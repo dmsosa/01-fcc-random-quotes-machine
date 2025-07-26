e140e07bfb5a2b4861ae9d682dfd18dbcfcca901
dmsosa <dmsosa21@outlook.com>
2025-07-26 11:48:39 +0200

    basename vite config corrected again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '01-fcc-random-quotes-machine',
+  base: '/01-fcc-random-quotes-machine/',
   plugins: [react()],
 })
