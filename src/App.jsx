c4f5abc63f21aa4417aad98f2ad0688d4f0c8c6d
dmsosa <dmsosa21@outlook.com>
2025-07-26 10:58:20 +0200

    Korrigieren

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import RandomQuotes from './components/RandomQuotes'
 
+export const THEMES_LENGTH = 5;
+
 function App() {
   const [ theme, setTheme ] = useState('theme-1');
+
   return (
-    <div id={`app-wrapper`} className={`app-wrapper ${theme}`}>
-      <RandomQuotes/>
+    <div id="app-wrapper" className={`app-wrapper ${theme}`}>
+      <RandomQuotes setTheme={setTheme}/>
     </div>
   )
 }
--- src/components/RandomQuotes.jsx
+++ src/components/RandomQuotes.jsx
@@ -1,16 +1,24 @@
 import { useEffect, useState } from "react";
-import { getRandomQuote } from "../service/quotesService";
+import { getQuoteArray } from "../service/quotesService";
 import { FaTumblr, FaTwitter } from "react-icons/fa";
+import { THEMES_LENGTH } from "../App";
 
-
-function RandomQuotes() {
-    const [ quote, setQuote ] = useState(undefined);
-    const [ status, setStatus ] = useState("loading");
+function changeTheme(setTheme) {
+    const randInt = Math.ceil(Math.random() * (THEMES_LENGTH - 1) );
+    setTheme(`theme-${randInt}`);
+}
+function RandomQuotes({ setTheme }) {
+    const [ { quoteArray, quoteIndex}, setQuoteState ] = useState({ quoteArray: undefined, quoteIndex: undefined });
+    const [ status, setStatus ] = useState('loading');
+    const { quote, author } = quoteArray ? quoteArray[quoteIndex] : {quote: undefined, author: undefined};
     useEffect(() => {
+
         setStatus('loading');
 
-        getRandomQuote().then((data) => {
-            setQuote(data);
+        getQuoteArray().then((data) => {
+            
+            const randIndex = Math.floor(Math.random() * (data.length));
+            setQuoteState({quoteArray: data, quoteIndex: randIndex });
             setStatus('fulfilled');
         })
         .catch((error) => {
@@ -19,28 +27,41 @@ function RandomQuotes() {
         })
     }, [])
 
+    const handleNewQuote = () => {
+        setQuoteState((prevState) => {
+            const prevIndex = prevState.quoteIndex;
+            let nextIndex;
+            if (prevIndex === quoteArray.length) {
+                nextIndex = 0;
+            } else {
+                nextIndex = prevIndex + 1;
+            }
+            return ({...prevState, quoteIndex:  nextIndex})
+        });
+        changeTheme(setTheme);
+    }
+
     return status === 'loading' ? 
         <div>Loading</div>
         :
         status === 'error' ?
         <div>Error </div>
         : 
-        <div id="quote-box" className="container bg-2 border border-width-1 box-shadow-1">
+        <div id="quote-box" className="container bg-2 border border-width-1 box-shadow-1 p-6">
             {/* Texte */}
-            <div className="">
-                <span className="fs-1">"</span>
-                <p id="text" className="">
-                    {quote.text}
+            <div>
+                <p id="text" className="h1 text-center">
+                    {`" ${quote}`}
                 </p>
-                <span  id="author" className="order-3 flex-basis-100 flex-grow-1">{'- ' + quote.author}</span>
+                <span id="author" className="h4 d-block text-right my-4">{`- ${author}.`}</span>
             </div>
             {/* Knopfe */}
             <div className="d-flex justify-content-between">
                 <div className="d-flex justify-content-center align-items-center gap-2">
-                    <FaTwitter id="tweet-quote" className="twitter-share-button" target="_blank" href={`https://twitter.com/intent/tweet?text=${`"${encodeURI(quote.text)}" -${quote.author}`}&hashtags=quotes,fccQuotes,quotesAPI`}></FaTwitter>
-                    <FaTumblr id="tumblr-quote" className="tumblr-share-button" target="_blank" href={`https://tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,fccQuotes,quotesAPI&content=${encodeURI(quote.text)}&caption=${quote.author}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons`}></FaTumblr>
+                    <a id="tweet-quote" className="twitter-share-button" target="_blank" href={`https://twitter.com/intent/tweet?text=${`"${encodeURI(quote)}" -${author}`}&hashtags=quotes,fccQuotes,quotesAPI`}><FaTwitter ></FaTwitter></a>
+                    <a id="tumblr-quote" className="tumblr-share-button" target="_blank" href={`https://tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,fccQuotes,quotesAPI&content=${encodeURI(quote)}&caption=${author}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons`}><FaTumblr></FaTumblr></a>
                 </div>
-                <button id="new-quote" className="btn btn-primary" onClick={() => setStatus('loading')}>New quote</button>
+                <button id="new-quote" className="btn btn-primary border-radius-0" onClick={handleNewQuote}>New quote</button>
             </div>
         </div>;
 }
